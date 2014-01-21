753ab73bd6108474edf09786ea2867b997539bcf
Chance Griffin <chance.griffin@gmail.com>
2014-01-21 17:59:07 -0600

    changed name of controller

--- test/controllers/quizzes_controller_test.rb
+++ test/controllers/quizzes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class StaticControllerTest < ActionController::TestCase
+class QuizzesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
