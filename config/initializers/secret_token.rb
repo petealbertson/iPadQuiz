b0e9eb4bffbca24748a4e57c585f8353e0f9fe5d
Pete Albertson <albertson.peter@gmail.com>
2014-01-21 15:50:24 -0600

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IPadQuiz::Application.config.secret_key_base = '1e13df15d799cf9022afab3c01047a92c2925d84c88942be8d932559f674d9ee6d2bfad8d30af1e2e4f5b1b9fa48d97a1c5e7613614bf550965741a9f9dc35ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IPadQuiz::Application.config.session_store :cookie_store, key: '_iPadQuiz_session'
